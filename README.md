d6214b87d09a2da483067c2015fd75d107c877b6
Jaxson <i@iiong.com>
2020-01-11 14:59:54 +0800

    💦更新说明

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
-# Ghost Theme Vue
+# Ghost Theme For Vue-Cli
 
-该脚手架实现在 `Ghost` 博客下生成 `Vue` 主题，并且拥有良好的 `seo` 支持。
+Use Vue-Cli to generate ghost theme.
 
-> 注意：本项目目前处于测试阶段，请勿在线上使用！
+---
+
+### 优点
+
+- 无需熟悉 `Ghost` 编写主题知识，仅需要 `Vue` 相关知识和 `Ghost Api`
+- 更现代化的主题开发体验
+- 快速开发精美主题
+- 拥有良好的 SEO
+- ...
+
+### 说明
+
+开发主题过程和 `Vue-Cli` 一样，如有问题请提 issue。
+
+### 手册
+
+开发过程请注意配置 `./src/theme.config.js`
+
+### 开发
+
+```bash
+npm run dev # 开发环境
+
+npm run build:ghost # 编译 Ghost 主题模板，打包 zip 上传即可
+
+npm run preview # 主题打包环境检测
+
+npm run lint # 语法检查
+```
+
+未完待续
+
+### License
+
+Ghost Theme For Vue-Cli is licensed under GNU General Public License Version 3 or later.
+
--- build/index.js
+++ build/index.js
@@ -1,16 +1,40 @@
 'use strict'
+const fs = require('fs')
 const { run } = require('runjs')
 const chalk = require('chalk')
 const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
 
+/**
+ * 文件操作
+ * @param inFile 输入文件
+ * @param outFile 输出文件
+ */
+const modifyFile = (inFile, outFile) => {
+  fs.readFile(`./${config.outputDir}/${inFile}`, 'utf8', (err, data) => {
+    if (err) {
+      throw err
+    }
+    const result = data.replace(/{{ghost_(head|foot)}}/g, ``)
+
+    fs.writeFile(`./${config.outputDir}/${outFile}`, result, 'utf8', (err) => {
+      if (err) {
+        throw err
+      }
+    })
+  })
+}
+
 if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   const report = rawArgv.includes('--report')
+  console.log(rawArgv)
 
   run(`vue-cli-service build ${args}`)
 
-  run(`node build/build.js`)
+  // 删除 ghost 模板引擎语法
+  modifyFile('index.html', 'index.html')
+  modifyFile('index.html', 'post.html')
 
   const port = 9526
   const publicPath = config.publicPath
